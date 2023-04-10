96ca267a1361d4099c349493c864349d5b2bde37
Adam Owada <adamowada@gmail.com>
2023-04-09 20:38:16 -0700

    changed date.py and added hello.py

--- api/date.py
+++ api/date.py
@@ -1,10 +1,12 @@
 from http.server import BaseHTTPRequestHandler
-from io import BytesIO
+from datetime import datetime
+
 
 class handler(BaseHTTPRequestHandler):
+
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        message = 'Hello from Python serverless function!'
-        self.wfile.write(message.encode())
+        self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+        return
