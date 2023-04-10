b36cfa372d34f5fba363efe73ad279205117a646
Adam Owada <adamowada@gmail.com>
2023-04-09 21:18:05 -0700

    made api/add https

--- api/add.py
+++ api/add.py
@@ -1,12 +1,15 @@
-from http.server import BaseHTTPRequestHandler
-import urllib.parse
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+from socketserver import ThreadingMixIn
 import json
+import ssl
 
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         # Get the two numbers from the query parameters
-        query_components = urllib.parse.parse_qs(urllib.parse.urlparse(self.path).query)
+        query_components = parse_qs(urlparse(self.path).query)
         num1 = int(query_components.get('num1', [0])[0])
         num2 = int(query_components.get('num2', [0])[0])
 
@@ -20,3 +23,21 @@ class handler(BaseHTTPRequestHandler):
         response = json.dumps({'result': result})
         self.wfile.write(response.encode())
         return
+
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
+
+
+if __name__ == '__main__':
+    server_address = ('', 8443)
+    httpd = ThreadedHTTPServer(server_address, handler)
+
+    # Set up SSL
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ssl_context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
+
+    httpd.socket = ssl_context.wrap_socket(httpd.socket, server_side=True)
+
+    print('Starting HTTPS server at https://localhost:8443')
+    httpd.serve_forever()
